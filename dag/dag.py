35c2ddfaa9831a1d5230f45e7d7295c285a211c9
VladBorisof <vl-borisof@yandex.ru>
2023-08-29 22:58:01 +0300

    add first dag

--- dag/dag.py
+++ dag/dag.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from airflow.utils.dates import days_ago
 from airflow.operators.dummy_operator import DummyOperator
 
-dag = DAG('dag',schedule_interval=timedelta(days=1), start_date=days_ago(1))
+dag = DAG('dag', schedule_interval=timedelta(days=1), start_date=days_ago(1))
 t1 = DummyOperator(task_id='task_1', dag=dag)
 t2 = DummyOperator(task_id='task_2',dag=dag)
 t3 = DummyOperator(task_id='task_3',dag=dag)
